6d04cec1a7e3c8fc1d03120067e2acfb6cf6f4d7
Syed Mustafa Hasan <107949586+sm0hasan@users.noreply.github.com>
2023-05-04 15:02:03 -0400

    Finished first three projects

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,30 @@
+// Set initial count
+let count = 0;
+
+// select value and buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(eventObject){
+        const styles = eventObject.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        } else if (styles.contains("increase")){
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if (count > 0){
+            value.style.color = "green"
+        } else if (count < 0) {
+            value.style.color = "red"
+        } else {
+            value.style.color = "black"
+        }
+
+        value.textContent = count;  
+    })
+})
--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -29,3 +29,54 @@ const reviews = [
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
+
+// Select Items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting items
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson(currentItem);
+});
+
+// Show person
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.author;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// Show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }  
+  showPerson(currentItem);
+})
+
+// Show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }  
+  showPerson(currentItem);
+})
+
+// Show random person
+randomBtn.addEventListener('click', function () {
+  let random = Math.floor(Math.random() * reviews.length);
+  showPerson(random);
+})
