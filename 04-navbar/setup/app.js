0e6cb830b9df3e8f92508e39c3d8bd96756c3f11
Syed Mustafa Hasan <107949586+sm0hasan@users.noreply.github.com>
2023-05-08 15:28:03 -0400

    Finished upto the 7th project

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,19 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener('click', function () {
+    // Long Method
+
+    // if (links.classList.contains("show-links")) {
+    //     links.classList.remove("show-links");
+    // } else {
+    //     links.classList.add("show-links");
+    // }
+
+    // Toggle Method
+    links.classList.toggle("show-links");
+});
--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,15 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+modalBtn = document.querySelector(".modal-btn");
+modalOverlay = document.querySelector(".modal-overlay");
+closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", function() {
+    modalOverlay.classList.add("open-modal");
+});
+
+closeBtn.addEventListener('click', function() {
+    modalOverlay.classList.remove('open-modal');
+});
